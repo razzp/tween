1d0d26699e698dc1b6f2f7e829641080a149efd9
razzp <64423130+razzp@users.noreply.github.com>
2023-01-26 16:45:25 +0000

    Annotations

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,13 @@
+/**
+ * Given a value, clamp it within a provided range.
+ */
 function clamp(value: number, min: number, max: number): number {
     return Math.min(Math.max(value, min), max);
 }
 
+/**
+ * Given a decimal between 0 and 1, find its value within a provided range.
+ */
 function toRange(value: number, min: number, max: number): number {
     return min + (max - min) * value;
 }
